1b8325ad0370fa03ad6279f00c62a69af403818f
Pattnatt <jittapat007@gmail.com>
2016-08-24 15:48:07 +0700

    Edit EnemySpawner
    - Add guaranteeSpawnTick to make sure Enemy will sapwn regulary.
    - Add lastSpawnColumn to track the lastest column that Enemyship
    spawned.
    - Separate spawnEnemy method into checkSpawn and spawnEnemy.

--- EnemySpawner.java
+++ EnemySpawner.java
@@ -4,11 +4,18 @@ public class EnemySpawner extends Actor
 {
     public int level = 0;
     private int levelUpTick = 600;
-    private int currentSpawnTick = 200;
-    private int tickPerLevel = 7;
-    private int tickCap = 30;
+    
+    private int currentSpawnTick = 600;
+    private int tickPerLevel = 10;
+    private int tickCap = 200;
+    
+    private int guaranteeSpawnTick = 200;
+    private int guaranteeTickPerLevel = 4;
+    private int guaranteeTickCap = 80;
+    
     private int tickPassed = 0;
     private int levelTickPassed = 0;
+    
     private int spawnPositionX[] = {100, 200, 300, 400, 500, 600, 700, 800};
     private int spawnPositionY = 90;
     
@@ -17,7 +24,7 @@ public class EnemySpawner extends Actor
     public void act() 
     {
         checkTick();
-        spawnEnemy();
+        checkSpawn();
     }
     
     private void checkTick()
@@ -35,23 +42,47 @@ public class EnemySpawner extends Actor
         level = level + 1;
         
         int tempTick = currentSpawnTick - tickPerLevel;
+        int guaranteeTemp = guaranteeSpawnTick - guaranteeTickPerLevel;
         if(tempTick < tickCap)
         {
             tempTick = tickCap;
         }
+        if(guaranteeTemp < guaranteeTickCap)
+        {
+            guaranteeTemp = guaranteeTickCap;
+        }
         
         currentSpawnTick = tempTick;
+        guaranteeSpawnTick = guaranteeTemp;
         levelTickPassed = 0;
+    }
+    
+    private void checkSpawn()
+    {
+        if(Greenfoot.getRandomNumber(currentSpawnTick) == 0)
+        {
+            spawnEnemy();
+        }
+        
+        if(tickPassed >= guaranteeSpawnTick)
+        {
+            spawnEnemy();
+            tickPassed = 0;
+        }
     }
     
     private void spawnEnemy()
     {
-        if(Greenfoot.getRandomNumber(currentSpawnTick) == 0)
+        int spawnColumn = Greenfoot.getRandomNumber(spawnPositionX.length - 1);
+        if(spawnColumn >= lastSpawnColumn)
         {
-            World world = getWorld();
-            EnemyShip enemyShip = new EnemyShip();
-            
-            world.addObject(enemyShip, spawnPositionX[ Greenfoot.getRandomNumber(spawnPositionX.length) ], spawnPositionY);
+            spawnColumn = spawnColumn + 1;
         }
+        
+        lastSpawnColumn = spawnColumn;
+        
+        World world = getWorld();
+        EnemyShip enemyShip = new EnemyShip();
+        world.addObject(enemyShip, spawnPositionX[spawnColumn], spawnPositionY);
     }
 }
